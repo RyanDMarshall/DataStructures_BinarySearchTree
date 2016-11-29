2ce47622e8e8496995e1e43125c0b78083bb9830
Ryan Marshall <ryanmars@umich.edu>
2016-11-29 01:29:02 -0500

    Re-uploading code because staff fucked up

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <iomanip>
 #include "BinarySearchTree.h"
 #include "Map.h"
 
 using namespace std;
 
 int main() {
-    
+
+    cout << setprecision(3);
 
     return 0;
 }
