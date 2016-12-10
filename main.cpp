55e09c926ea75f0b5e7dcd3bbc10d1918485d93e
Ryan Marshall <ryanmars@umich.edu>
2016-12-10 17:36:50 -0500

    Maps all data as (Key = word, Value = category) pairs, and counts total number of words and total number of posts, as well as number of posts of each category

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
-#include <fstream>
 #include <iomanip>
+#include <fstream>
 #include <string>
+#include <cmath>
 #include "csvstream.h"
 #include "map"
 #include "set"
 
 using namespace std;
 
-map<string, string> map_classifier(int& word_count, int& post_count);
-//map<string, double> map_probability(int word_count, int post_count);
+map<string, string> map_classifier(int& word_count, int& post_count, 
+    	int& calculator_count, int& euchre_count, int& exam_count,
+    	int& image_count, int& recursion_count, int& statistics_count);
+map<string, double> map_probability(int& word_count, int& post_count, 
+    	int& calculator_count, int& euchre_count, int& exam_count,
+    	int& image_count, int& recursion_count, int& statistics_count);
 
 
 set<string> unique_words(const string &str);
@@ -20,12 +25,32 @@ int main() {
 
     int word_count = 0;
     int post_count = 0;
-    map<string, string> classifier = map_classifier(word_count, post_count);
+    int calculator_count = 0;
+    int euchre_count = 0;
+    int exam_count = 0;
+    int image_count = 0;
+    int recursion_count = 0;
+    int statistics_count = 0;
 
-    return 0;
+    map<string, string> classifier = map_classifier(word_count, 
+    	post_count, calculator_count, euchre_count, exam_count, 
+    	image_count, recursion_count, statistics_count);
+
+    /*cout << "Word Count = " << word_count << endl
+    		 << "Post Count = " << post_count << endl
+    		 << "Calculator Count = " << calculator_count << endl
+    		 << "Euchre Count = " << euchre_count << endl
+    		 << "Exam Count = " << exam_count << endl
+    		 << "Image Count = " << image_count << endl
+    		 << "Recursion Count = " << recursion_count << endl
+    		 << "Statistics Count = " << statistics_count << endl;*/
+
+    		     return 0;
 }
 
-map<string, string> map_classifier(int& word_count, int& post_count) {
+map<string, string> map_classifier(int& word_count, int& post_count, 
+    	int& calculator_count, int& euchre_count, int& exam_count,
+    	int& image_count, int& recursion_count, int& statistics_count) {
 
 	map<string, string> classifier;
 
@@ -45,9 +70,28 @@ map<string, string> map_classifier(int& word_count, int& post_count) {
  	set<string> total_class;
 	
  	while (train_small >> row || test_small >> row 
- 	|| w16_projects_exam >> row || sp16_projects_exam >> row) {
+ 		|| w16_projects_exam >> row || sp16_projects_exam >> row) {
 
  		++post_count;
+ 		if (row["tag"] == "calculator") {
+ 			++calculator_count;
+ 		}
+ 		if (row["tag"] == "euchre") {
+ 			++euchre_count;
+ 		}
+  	if (row["tag"] == "exam") {
+ 			++exam_count;
+ 		}
+ 		if (row["tag"] == "image") {
+ 			++image_count;
+ 		}
+ 		if (row["tag"] == "recursion") {
+ 			++recursion_count;
+ 		}
+ 		if (row["tag"] == "statistics") {
+ 			++statistics_count;
+ 		}
+
 
  		all_word.append(row["content"]);
  		all_class.append(row["tag"]);
@@ -60,8 +104,8 @@ map<string, string> map_classifier(int& word_count, int& post_count) {
  		while (current_iter != current_content.end()) {
 
  			pair<string, string> insert_pair;
- 			insert_pair.first = row["tag"];
- 			insert_pair.second = *current_iter;
+ 			insert_pair.first = *current_iter;
+ 			insert_pair.second = row["tag"];
 
  			classifier.insert(insert_pair);
 
@@ -77,15 +121,21 @@ map<string, string> map_classifier(int& word_count, int& post_count) {
 
 	word_count = total_word.size();
 
-	cout << endl << "Word Count = " << word_count 
-			 << endl << "Post Count = " << post_count; 
-
-	cout << endl << "Map Size = " << classifier.size() << endl;
+	while (word_iter != total_word.end()) {
+		cout << *word_iter << endl;
+		++word_iter;
+	} cout << endl << endl;
+	while (classify_iter != total_class.end()) {
+		cout << *classify_iter << endl;
+		++classify_iter;
+	} cout << endl << endl;
 
 	return classifier;
 }
 
-/*map<string, double> map_probability() {
+/*map<string, double> map_probability(int& word_count, int& post_count, 
+    	int& calculator_count, int& euchre_count, int& exam_count,
+    	int& image_count, int& recursion_count, int& statistics_count) {
 
 	map<string, int> counter = map_counter();
 	map<string, double> probability;
