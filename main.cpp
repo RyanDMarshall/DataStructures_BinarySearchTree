c7cdc4b07867e069c26b0549a25afd13d57def15
Ryan Marshall <ryanmars@umich.edu>
2016-12-10 19:18:00 -0500

    Separate functions for classification by subject and by author, skeleton for functions to calculate probability

--- main.cpp
+++ main.cpp
@@ -9,58 +9,83 @@
 
 using namespace std;
 
-map<string, string> map_classifier(int& word_count, int& post_count, 
-    	int& calculator_count, int& euchre_count, int& exam_count,
-    	int& image_count, int& recursion_count, int& statistics_count);
-map<string, double> map_probability(int& word_count, int& post_count, 
+map<string, string> trainer_by_subject(int& word_count, int& post_count, 
     	int& calculator_count, int& euchre_count, int& exam_count,
     	int& image_count, int& recursion_count, int& statistics_count);
 
+map<string, string> trainer_by_author(int& word_count, int& post_count, 
+    	int& student_count, int& instructor_count);
+
+double probability_by_subject(string word, int word_count, int post_count, 
+    	int calculator_count, int euchre_count, int exam_count,
+    	int image_count, int recursion_count, int statistics_count);
+
+double probability_by_author(string word, int word_count, int post_count, 
+    	int student_count, int instructor_count);
 
 set<string> unique_words(const string &str);
 
+
+
 int main() {
 
-    cout << setprecision(3);
+  cout << setprecision(3);
 
-    int word_count = 0;
-    int post_count = 0;
-    int calculator_count = 0;
-    int euchre_count = 0;
-    int exam_count = 0;
-    int image_count = 0;
-    int recursion_count = 0;
-    int statistics_count = 0;
+  int word_count = 0;
+  int post_count = 0;
+  int calculator_count = 0;
+  int euchre_count = 0;
+  int exam_count = 0;
+  int image_count = 0;
+  int recursion_count = 0;
+  int statistics_count = 0;
+  int student_count = 0;
+  int instructor_count = 0;
 
-    map<string, string> classifier = map_classifier(word_count, 
-    	post_count, calculator_count, euchre_count, exam_count, 
-    	image_count, recursion_count, statistics_count);
+  map<string, string> classifier_by_subject = 
+  	trainer_by_subject(word_count, post_count, calculator_count, 
+  	euchre_count, exam_count, image_count, recursion_count, statistics_count);
 
-    /*cout << "Word Count = " << word_count << endl
-    		 << "Post Count = " << post_count << endl
-    		 << "Calculator Count = " << calculator_count << endl
-    		 << "Euchre Count = " << euchre_count << endl
-    		 << "Exam Count = " << exam_count << endl
-    		 << "Image Count = " << image_count << endl
-    		 << "Recursion Count = " << recursion_count << endl
-    		 << "Statistics Count = " << statistics_count << endl;*/
+  map<string, string> classifier_by_author = trainer_by_author(word_count,
+  post_count, student_count, instructor_count);
 
-    		     return 0;
+ 	string word_test;
+ 	cout << "Enter a word dumbass:" << endl;
+ 	cin >> word_test;
+
+ 	double prob_by_subject = 0;
+ 	double prob_by_author = 0;
+
+ 	prob_by_subject = probability_by_subject(word_test, word_count, post_count, 
+ 		calculator_count, euchre_count, exam_count, image_count, 
+ 		recursion_count, statistics_count);
+
+ 	prob_by_author = probability_by_author(word_test, word_count, post_count, 
+ 		student_count, instructor_count);
+
+ 	cout << prob_by_subject << endl << prob_by_author << endl;
+
+  /*cout << "Word Count = " << word_count << endl
+  		 << "Post Count = " << post_count << endl
+  		 << "Calculator Count = " << calculator_count << endl
+  		 << "Euchre Count = " << euchre_count << endl
+  		 << "Exam Count = " << exam_count << endl
+  		 << "Image Count = " << image_count << endl
+  		 << "Recursion Count = " << recursion_count << endl
+  		 << "Statistics Count = " << statistics_count << endl
+  		 << "Student Count = " << student_count << endl
+  		 << "Instructor Count = " << instructor_count << endl;*/
+
+  return 0;
 }
 
-map<string, string> map_classifier(int& word_count, int& post_count, 
+map<string, string> trainer_by_subject(int& word_count, int& post_count, 
     	int& calculator_count, int& euchre_count, int& exam_count,
     	int& image_count, int& recursion_count, int& statistics_count) {
 
 	map<string, string> classifier;
 
-  csvstream train_small("train_small.csv");
-  csvstream test_small("test_small.csv", ',');
   csvstream w16_projects_exam("w16_projects_exam.csv");
-  csvstream sp16_projects_exam("sp16_projects_exam.csv");
-  csvstream w14_f15_instructor_student("w14-f15_instructor_student.csv");
-  csvstream w16_instructor_student("w16_instructor_student.csv");
-
  	csvstream::row_type row;
 
  	string all_word;
@@ -69,8 +94,7 @@ map<string, string> map_classifier(int& word_count, int& post_count,
  	set<string> total_word;
  	set<string> total_class;
 	
- 	while (train_small >> row || test_small >> row 
- 		|| w16_projects_exam >> row || sp16_projects_exam >> row) {
+ 	while (w16_projects_exam >> row) {
 
  		++post_count;
  		if (row["tag"] == "calculator") {
@@ -92,16 +116,15 @@ map<string, string> map_classifier(int& word_count, int& post_count,
  			++statistics_count;
  		}
 
+		all_word.append(row["content"]);
+		all_class.append(row["tag"]);
+		all_word.append(" ");
+		all_class.append(" ");
 
- 		all_word.append(row["content"]);
- 		all_class.append(row["tag"]);
- 		all_word.append(" ");
- 		all_class.append(" ");
+		set<basic_string<char> > current_content = unique_words(row["content"]);
+		set<basic_string<char> >::iterator current_iter = current_content.begin();
 
- 		set<basic_string<char> > current_content = unique_words(row["content"]);
- 		set<basic_string<char> >::iterator current_iter = current_content.begin();
-
- 		while (current_iter != current_content.end()) {
+	 	while (current_iter != current_content.end()) {
 
  			pair<string, string> insert_pair;
  			insert_pair.first = *current_iter;
@@ -119,37 +142,106 @@ map<string, string> map_classifier(int& word_count, int& post_count,
   set<basic_string<char> >::iterator word_iter = total_word.begin();    
   set<basic_string<char> >::iterator classify_iter = total_class.begin();
 
-	word_count = total_word.size();
+	word_count += total_word.size();
 
-	while (word_iter != total_word.end()) {
+	/*while (word_iter != total_word.end()) {
 		cout << *word_iter << endl;
 		++word_iter;
 	} cout << endl << endl;
 	while (classify_iter != total_class.end()) {
 		cout << *classify_iter << endl;
 		++classify_iter;
-	} cout << endl << endl;
+	} cout << endl << endl;*/
 
 	return classifier;
 }
 
-/*map<string, double> map_probability(int& word_count, int& post_count, 
-    	int& calculator_count, int& euchre_count, int& exam_count,
-    	int& image_count, int& recursion_count, int& statistics_count) {
+map<string, string> trainer_by_author(int& word_count, 
+	int& post_count, int& student_count, int& instructor_count) {
 
-	map<string, int> counter = map_counter();
-	map<string, double> probability;
+	map<string, string> classifier;
 
-	return probability;
-}*/
+  csvstream w14_f15_instructor_student("w14-f15_instructor_student.csv");
+ 	csvstream::row_type row;
+
+ 	string all_word;
+ 	string all_class;
+
+ 	set<string> total_word;
+ 	set<string> total_class;
+	
+ 	while (w14_f15_instructor_student >> row) {
+
+ 		++post_count;
+ 		if (row["tag"] == "student") {
+ 			++student_count;
+ 		}
+ 		if (row["tag"] == "instructor") {
+ 			++instructor_count;
+ 		}
+
+		all_word.append(row["content"]);
+		all_class.append(row["tag"]);
+		all_word.append(" ");
+		all_class.append(" ");
+
+		set<basic_string<char> > current_content = unique_words(row["content"]);
+		set<basic_string<char> >::iterator current_iter = current_content.begin();
+
+	 	while (current_iter != current_content.end()) {
+
+ 			pair<string, string> insert_pair;
+ 			insert_pair.first = *current_iter;
+ 			insert_pair.second = row["tag"];
+
+ 			classifier.insert(insert_pair);
+
+ 			++current_iter;
+ 		}
+	}
+
+	total_word = unique_words(all_word);
+	total_class = unique_words(all_class);
+
+  set<basic_string<char> >::iterator word_iter = total_word.begin();    
+  set<basic_string<char> >::iterator classify_iter = total_class.begin();
+
+	word_count += total_word.size();
+
+	/*while (word_iter != total_word.end()) {
+		cout << *word_iter << endl;
+		++word_iter;
+	} cout << endl << endl;
+	while (classify_iter != total_class.end()) {
+		cout << *classify_iter << endl;
+		++classify_iter;
+	} cout << endl << endl;*/
+
+	return classifier;
+}
+
+double probability_by_subject(string word, int word_count, int post_count, 
+    	int calculator_count, int euchre_count, int exam_count,
+    	int image_count, int recursion_count, int statistics_count) {
+
+	double probability_calc = 0;
+
+	return probability_calc;
+}
+
+double probability_by_author(string word, int word_count, int post_count, 
+    	int student_count, int instructor_count) {
+
+	double probability_calc = 0;
+
+	return probability_calc;
+}
 
 set<string> unique_words(const string &str) {
 
 	istringstream source(str);
 	set<string> words;
 	string word;
-
-	// Read word by word from the stringstream and insert into the set
 
 	while (source >> word) {
 		words.insert(word);
