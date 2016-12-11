98f6eb316d97327ac828b197a6f9875783f06cb3
Ryan Marshall <ryanmars@umich.edu>
2016-12-10 21:01:39 -0500

    All basics implemented. Just need probability functions

--- main.cpp
+++ main.cpp
@@ -12,27 +12,28 @@ using namespace std;
 map<string, string> trainer_by_subject(int& word_count, int& post_count, 
     	int& calculator_count, int& euchre_count, int& exam_count,
     	int& image_count, int& recursion_count, int& statistics_count);
-
 map<string, string> trainer_by_author(int& word_count, int& post_count, 
     	int& student_count, int& instructor_count);
 
 double probability_by_subject(string word, int word_count, int post_count, 
     	int calculator_count, int euchre_count, int exam_count,
     	int image_count, int recursion_count, int statistics_count);
-
 double probability_by_author(string word, int word_count, int post_count, 
     	int student_count, int instructor_count);
 
+int word_counter_given_label(string word, string label);
+int total_word_counter(string word);
+
 set<string> unique_words(const string &str);
-
-
 
 int main() {
 
   cout << setprecision(3);
 
-  int word_count = 0;
-  int post_count = 0;
+  int word_count_by_subject = 0;
+  int post_count_by_subject = 0;
+  int word_count_by_author = 0;
+  int post_count_by_author = 0;
   int calculator_count = 0;
   int euchre_count = 0;
   int exam_count = 0;
@@ -43,30 +44,32 @@ int main() {
   int instructor_count = 0;
 
   map<string, string> classifier_by_subject = 
-  	trainer_by_subject(word_count, post_count, calculator_count, 
+  	trainer_by_subject(word_count_by_subject, post_count_by_subject, calculator_count, 
   	euchre_count, exam_count, image_count, recursion_count, statistics_count);
 
-  map<string, string> classifier_by_author = trainer_by_author(word_count,
-  post_count, student_count, instructor_count);
+  map<string, string> classifier_by_author = trainer_by_author(word_count_by_author,
+  	post_count_by_author, student_count, instructor_count);
 
- 	string word_test;
- 	cout << "Enter a word dumbass:" << endl;
+ 	string word_test, label_test;
+ 	cout << "Enter a word, dumbass:" << endl;
  	cin >> word_test;
+ 	cout << "Enter a god Damn label" << endl;
+ 	cin >> label_test;
 
  	double prob_by_subject = 0;
  	double prob_by_author = 0;
 
- 	prob_by_subject = probability_by_subject(word_test, word_count, post_count, 
- 		calculator_count, euchre_count, exam_count, image_count, 
+ 	prob_by_subject = probability_by_subject(word_test, word_count_by_subject,
+ 		post_count_by_subject, calculator_count, euchre_count, exam_count, image_count, 
  		recursion_count, statistics_count);
 
- 	prob_by_author = probability_by_author(word_test, word_count, post_count, 
- 		student_count, instructor_count);
+ 	prob_by_author = probability_by_author(word_test, word_count_by_author,
+ 	  post_count_by_author, student_count, instructor_count);
 
- 	cout << prob_by_subject << endl << prob_by_author << endl;
-
-  /*cout << "Word Count = " << word_count << endl
-  		 << "Post Count = " << post_count << endl
+  cout << "Word Count By Subject = " << word_count_by_subject << endl
+  		 << "Word Count By Author = " << word_count_by_author << endl
+  		 << "Post Count By Subject = " << post_count_by_subject << endl
+  		 << "Post Count By Author = " << post_count_by_author << endl
   		 << "Calculator Count = " << calculator_count << endl
   		 << "Euchre Count = " << euchre_count << endl
   		 << "Exam Count = " << exam_count << endl
@@ -74,14 +77,18 @@ int main() {
   		 << "Recursion Count = " << recursion_count << endl
   		 << "Statistics Count = " << statistics_count << endl
   		 << "Student Count = " << student_count << endl
-  		 << "Instructor Count = " << instructor_count << endl;*/
+  		 << "Instructor Count = " << instructor_count << endl
+  		 << "Word Count Given Label = " << word_counter_given_label(word_test, label_test) << endl
+  		 << "Total Word Count = " << total_word_counter(word_test) << endl
+			 << "Probability by subject = " << prob_by_subject << endl
+			 << "Probability by author = " << prob_by_author << endl;
 
   return 0;
 }
 
 map<string, string> trainer_by_subject(int& word_count, int& post_count, 
-    	int& calculator_count, int& euchre_count, int& exam_count,
-    	int& image_count, int& recursion_count, int& statistics_count) {
+  	int& calculator_count, int& euchre_count, int& exam_count,
+  	int& image_count, int& recursion_count, int& statistics_count) {
 
 	map<string, string> classifier;
 
@@ -144,15 +151,6 @@ map<string, string> trainer_by_subject(int& word_count, int& post_count,
 
 	word_count += total_word.size();
 
-	/*while (word_iter != total_word.end()) {
-		cout << *word_iter << endl;
-		++word_iter;
-	} cout << endl << endl;
-	while (classify_iter != total_class.end()) {
-		cout << *classify_iter << endl;
-		++classify_iter;
-	} cout << endl << endl;*/
-
 	return classifier;
 }
 
@@ -208,15 +206,6 @@ map<string, string> trainer_by_author(int& word_count,
 
 	word_count += total_word.size();
 
-	/*while (word_iter != total_word.end()) {
-		cout << *word_iter << endl;
-		++word_iter;
-	} cout << endl << endl;
-	while (classify_iter != total_class.end()) {
-		cout << *classify_iter << endl;
-		++classify_iter;
-	} cout << endl << endl;*/
-
 	return classifier;
 }
 
@@ -235,6 +224,42 @@ double probability_by_author(string word, int word_count, int post_count,
 	double probability_calc = 0;
 
 	return probability_calc;
+}
+
+int word_counter_given_label(string word, string label) {
+
+	int word_counter = 0;
+
+  csvstream w14_f15_instructor_student("w14-f15_instructor_student.csv");
+	csvstream w16_projects_exam("w16_projects_exam.csv");
+ 	csvstream::row_type row;
+
+ 	set<string> current_string;
+
+	while (w14_f15_instructor_student >> row || w16_projects_exam >> row) {
+		if (row["tag"] == label) {
+			current_string = unique_words(row["content"]);
+
+			if (current_string.find(word) != current_string.end()) {
+				++word_counter;
+			}
+		}
+	}
+
+	return word_counter;
+}
+
+int total_word_counter(string word) {
+
+	int total_count = 0;
+	vector<string> labels = {"students", "instructors", "calculator", 
+	"euchre", "exam", "image", "recursion", "statistics"};
+
+	for (int i = 0; i < int(labels.size()); ++i) {
+		total_count += word_counter_given_label(word, labels.at(i));
+	}
+
+	return total_count;
 }
 
 set<string> unique_words(const string &str) {
